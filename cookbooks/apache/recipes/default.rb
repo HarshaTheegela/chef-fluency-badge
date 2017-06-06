4b3f8b1e4bbe911ca94710ceab53a7357a15a474
HarshaTheegela <chandraharsha.theegela@gmail.com>
2017-06-06 20:52:36 +0000

    Added basic apache cookbook

---
+++ cookbooks/apache/recipes/default.rb
@@ -0,0 +1,18 @@
+#
+# Cookbook:: apache
+# Recipe:: default
+#
+
+# Copyright:: 2017, The Authors, All Rights Reserved.
+#install apache package
+package 'apache' do
+	package_name 'httpd'
+	action :install
+end
+
+#start the httpd
+service 'apache2' do
+	service_name 'httpd'
+	action[:start, :enable]
+end
+
