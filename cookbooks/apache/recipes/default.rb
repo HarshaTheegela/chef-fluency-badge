da537b86feeebaff44b24d951019041de7bde71b
HarshaTheegela <chandraharsha.theegela@gmail.com>
2017-06-08 01:58:52 +0000

    new changes

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -4,15 +4,21 @@
 #
 
 # Copyright:: 2017, The Authors, All Rights Reserved.
+if node ['platform_family'] == 'rhel'
+	package ="httpd"
+elsif node ['platfomr_family'] == "debian"
+	package ="apache2"
+end
 #install apache package
-package 'apache' do
-	package_name 'httpd'
+package 'httpd' do
+	package_name package
 	action :install
 end
 
 #start the httpd
-service 'apache2' do
-	service_name 'httpd'
+service 'httpd' do
+	service_name package
 	action [:start, :enable]
 end
 
+#include_recipe 'apache::websites'
